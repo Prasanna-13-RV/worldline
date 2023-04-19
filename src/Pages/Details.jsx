63d6f7695305cc17f6e5743a814af655ae04966b
Prasanna RV <prasannavelmurugan0200@gmail.com>
2023-04-19 11:51:32 +0530

    rv

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, {useEffect, useState} from "react";
+import "../Files/css/Details.css"
 
 function Details() {
 	const [details, setDetails] = useState([]);
@@ -10,57 +11,58 @@ function Details() {
 		});
 	}, []);
 
+
 	return (
 		<>
-			<div>{JSON.stringify(details)}</div>
-			<table>
-				<thead>
-					<th>
-						<td>Employee Name</td>
-					</th>
-					<th>
-						<td>Mail</td>
-					</th>
-					<th>
-						<td>Phone</td>
-					</th>
-					<th>
-						<td>Gender</td>
-					</th>
-					<th>
-						<td>Qualification</td>
-					</th>
-					<th>
-						<td>Experience</td>
-					</th>
-
-					<th>
-						<td>Past Experience</td>
-					</th>
-					<th>
-						<td>Current Experience</td>
-					</th>
-				</thead>
-				<tbody>
-					{details.forEach((e, index) => {
-						return (
-							<>
-								<p>{console.log(e.e_name)}</p>
-								{/* <tr>
-									<td>{e.e_name}</td>
-									<td>Hello</td>
-									<td>Hello</td>
-									<td>Hello</td>
-									<td>Hello</td>
-									<td>Hello</td>
-									<td>Hello</td>
-									<td>Hello</td>
-								</tr> */}
-							</>
-						);
-					})}
-				</tbody>
-			</table>
+			<section className="section">
+                <h1>Employee Details</h1>
+				<table>
+					<thead>
+						<th>
+							<td>Employee Name</td>
+						</th>
+						<th>
+							<td>Mail</td>
+						</th>
+						<th>
+							<td>Phone</td>
+						</th>
+						<th>
+							<td>Gender</td>
+						</th>
+						<th>
+							<td>Qualification</td>
+						</th>
+						<th>
+							<td>Experience</td>
+						</th>
+						<th>
+							<td>Past Experience</td>
+						</th>
+						<th>
+							<td>Current Experience</td>
+						</th>
+					</thead>
+					<tbody>
+						{details.map((e, index) => {
+							return (
+								<>
+									<tr>
+										<td>{e.e_name}</td>
+										<td>{e.e_email}</td>
+										<td>{e.e_phone}</td>
+										<td>{e.e_gender}</td>
+										<td>{e.e_past_experience}</td>
+										<td>{e.e_data_joined.slice(0, 10)}</td>
+										<td>{e.e_dob.slice(0, 10)}</td>
+										<td>{e.e_data_joined.slice(0, 10) - "2023-04-19"}</td>
+									</tr>
+								</>
+							);
+						})}
+					</tbody>
+				</table>
+			</section>
 		</>
 	);
 }
